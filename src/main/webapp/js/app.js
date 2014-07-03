f26ca637294138c5222c1e686d9163968d62e7ba
p1312018 <benaounmhemed@gmail.com>
2014-07-03 16:03:31 +0200

    Développement de la partie client en utilisant les ressources Rest exposé par notre serveur.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,7 +1,27 @@
 var app = angular.module("geektic", ['ngRoute']);
 
 app.controller('HelloCtrl', function($scope, $http) {
-    $http.get('/api/hello').success(function(helloMessage) {
-        $scope.helloMessage = helloMessage;
-    });
+	$scope.alimenterComboInterets = function()
+	{
+		$http.get('/geek/interetsListe').success(function(interets) {
+            $scope.interets = interets;
+            $scope.interet = interets[0];
+        });
+	}
+	
+	$scope.alimenterComboInterets();
+	
+	$scope.trouverGeeks = function()
+    {
+		if ($scope.sexe == "" || $scope.sexe == null)
+			{
+				alert("Vous avez oubliez de selectionner un sex !!!!!");
+				return;
+			}
+		else {
+			$http.get('/geek/' + $scope.sexe + '/' + $scope.interet.unIdInteret).success(function(geeks) {
+	            $scope.geeks = geeks;
+	        });
+		}
+    };    
 });
